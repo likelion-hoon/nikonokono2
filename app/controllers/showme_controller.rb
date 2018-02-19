2b0e167b7fdc5dfa85336f30996253521dd18285
hoon <monki0120@naver.com>
2018-02-19 16:37:10 +0900

    board_write_form grid 설정

--- app/controllers/showme_controller.rb
+++ app/controllers/showme_controller.rb
@@ -1,10 +1,18 @@
 class ShowmeController < ApplicationController
   def board
-
+    @posts = Post.all.order('id desc')
   end
 
   def board_write
+    @post = Post.new
+    @post.title = params[:title]
+    @post.content = params[:content]
+    @post.singer = params[:singer]
+    @post.song = params[:song]
 
+    @post.save
+
+    redirect_to "/showme/board"
   end
 
   def board_write_form
---
+++ db/migrate/20180219063410_create_posts.rb
@@ -0,0 +1,17 @@
+class CreatePosts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :posts do |t|
+
+      t.string :title   # 글 제목
+      t.text :content # 글 내용
+      t.string :singer   # 가수 이름
+      t.string :song     # 노래 제목
+      t.integer :hit, default: 0     # 조회 수
+      t.integer :recom, default: 0   # 추천 수
+
+      # t.references :user, index: true, foreign_key: true # user 외래키 자리
+
+      t.timestamps
+    end
+  end
+end
