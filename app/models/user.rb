9847a69a5713e38edbd49cc9d83ffea1add7688a
hoon <monki0120@naver.com>
2018-03-10 23:38:07 +0900

    omniauth-google-oauth2 로그인 성공

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
      end
    end
 
+   # facebook login 위한 코드
    def self.from_omniauth(auth)
      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
        user.email = auth.info.email
@@ -21,4 +22,19 @@ class User < ApplicationRecord
        user.image = auth.info.image # assuming the user model has an image
      end
    end
+
+   # google login 위한 코드
+   def self.from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(email: data['email']).first
+
+      # Uncomment the section below if you want users to be created if they don't exist
+      # unless user
+      #     user = User.create(name: data['name'],
+      #        email: data['email'],
+      #        password: Devise.friendly_token[0,20]
+      #     )
+      # end
+      user
+    end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   config.omniauth :facebook, ENV["Facebook_Key"], ENV["Facebook_Secret"], callback_url: "http://localhost:3000/users/auth/facebook/callback"
-  config.omniauth :google_oauth2, ENV["Google_Key"], ENV["Google_Secret"], callback_url: "http://localhost:3000/map/index"
+  config.omniauth :google_oauth2, ENV["Google_Key"], ENV["Google_Secret"], callback_url: "http://localhost:3000/users/auth/google_oauth2/callback"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
